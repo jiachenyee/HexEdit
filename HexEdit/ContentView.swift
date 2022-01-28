2a67eba245b9b0bc228854283edd6d03e1eaacbd
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-01-28 13:01:55 +0800

    fix issue where typing in became strange because it kept trying to generate hex

--- HexEdit/ContentView.swift
+++ HexEdit/ContentView.swift
@@ -12,46 +12,46 @@ struct ContentView: View {
     @Binding var document: HexEditDocument
     @State var addMenuPresented = false
     
+    @State var inputText = ""
+    
     var body: some View {
-        
-        let text = Binding<String>(
-            get: {
-                document.data.reduce(into: "") { acc, byte in
+        TextEditor(text: $inputText)
+            .font(.system(.body, design: .monospaced))
+            .onChange(of: inputText) { newValue in
+                inputText = newValue.filter { $0.isHexDigit }.separate(every: 2, with: " ")
+                
+                document.data = newValue.filter {
+                    $0.isHexDigit
+                }.hexadecimal ?? Data()
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .navigation) {
+                    Button {
+                        addMenuPresented = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .popover(isPresented: $addMenuPresented) {
+                        AddTextView { str in
+                            document.data.append(contentsOf: str)
+                        }
+                    }
+                }
+            }
+            .onAppear {
+                inputText = document.data.reduce(into: "") { acc, byte in
                     var s = String(byte, radix: 16)
                     if s.count == 1 {
                         s = "0" + s
                     }
                     acc += s + " "
                 }
-            },
-            set: { value in
-                document.data = value.filter {
-                    $0.isHexDigit
-                }.hexadecimal ?? Data()
             }
-        )
-        
-        NavigationView {
-            Text("Hello")
-            TextEditor(text: text)
-                .font(.system(.body, design: .monospaced))
-        }
-        .toolbar {
-            ToolbarItem(placement: .navigation) {
-                Button {
-                    addMenuPresented = true
-                } label: {
-                    Image(systemName: "plus")
-                }
-                .popover(isPresented: $addMenuPresented) {
-                    AddTextView { str in
-                        document.data.append(contentsOf: str)
-                    }
-                }
-            }
-        }
-        
     }
+    func toggleSidebar() {
+        NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+    }
+    
 }
 
 //struct ContentView_Previews: PreviewProvider {
@@ -83,4 +83,10 @@ extension String {
         return data
     }
     
+}
+
+extension String {
+    func separate(every stride: Int = 4, with separator: Character = " ") -> String {
+        return String(enumerated().map { $0 > 0 && $0 % stride == 0 ? [separator, $1] : [$1]}.joined())
+    }
 }
